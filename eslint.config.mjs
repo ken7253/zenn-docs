75a110e4503a86933d7a85f4af20bf8b28697cb6
ken7253 <ken7253@gmail.com>
2024-08-12 16:04:47 +0900

    markdownに対してeslintを効かせる

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import markdown from "eslint-plugin-markdown";
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  {
+    name: "eslint/setup/globals",
+    languageOptions: {
+      globals: { ...globals.node, ...globals.browser },
+    },
+  },
+  ...markdown.configs.recommended,
+  {
+    name: "eslint/recommended",
+    files: ["**/*.md/*.js", "**/*.(m|c)?js"],
+    ...pluginJs.configs.recommended,
+  },
   ...tseslint.configs.recommended,
-];
+);
